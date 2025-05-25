c4468b2e0614a2c7e87a68fe82fc291f5e13ae53
Stiven-Richardy <stivenrichardy445@gmail.com>
2025-05-25 12:25:41 -0300

    Criação da classe TP03 e funcionalidades do código

--- Hora.java
+++ Hora.java
@@ -20,34 +20,22 @@ public class Hora {
     }
 
     public Hora(int hora, int minuto, int segundo) {
-        this.hora = hora;
-        this.minuto = minuto;
-        this.segundo = segundo;
+        this.setHora(hora);
+        this.setMinuto(minuto);
+        this.setSegundo(segundo);
     }
 
     //Setters
     public void setHora(int hora) {
-        if (hora >= 0 && hora < 24) {
-            this.hora = hora;
-        } else {
-            System.out.println("Hora inválida. Deve estar entre 0 e 23.");
-        }
+        this.hora = hora;
     }
 
     public void setMinuto(int minuto) {
-        if (minuto >= 0 && minuto < 60) {
-            this.minuto = minuto;
-        } else {
-            System.out.println("Minuto inválido. Deve estar entre 0 e 59.");
-        }
+        this.minuto = minuto;
     }
 
     public void setSegundo(int segundo) {
-        if (segundo >= 0 && segundo < 60) {
-            this.segundo = segundo;
-        } else {
-            System.out.println("Segundo inválido. Deve estar entre 0 e 59.");
-        }
+        this.segundo = segundo;
     }
 
     public void setHora() {
@@ -77,14 +65,16 @@ public class Hora {
 
     //Métodos
     public String getHora1() {
-        return "";
+        return String.format("%02d:%02d:%02d", this.hora, this.minuto, this.segundo);
     }
 
     public String getHora2() {
-        return "";
+        return String.format("%02d:%02d:%02d %s", 
+            (this.hora % 12 == 0) ? 12 : this.hora % 12, this.minuto, this.segundo, 
+            (this.hora < 12) ? "AM" : "PM");
     }
 
     public int getSegundos() {
-        return 0;
+        return hora * 3600 + minuto * 60 + segundo;
     }
 }
--- TP03.java
+++ TP03.java
@@ -7,9 +7,81 @@ INTEGRANTES:
 */
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class TP03 {
+	public static int lerHora(Scanner ler) {
+		int hora = 0;
+
+		while(true) {
+			try {
+				System.out.print("Digite a hora (0-23): ");
+				hora = ler.nextInt();
+				if (hora < 0 || hora > 23) {
+					System.out.println("Hora inválida. Deve estar entre 0 e 23. Tente novamente:");
+				} else {
+					return hora;
+				}
+			} catch (InputMismatchException error) {
+				System.out.println("Digite um número inteiro válido.");
+				ler.next();
+			}
+		}
+	}
+
+	public static int lerMinuto(Scanner ler) {
+		int minuto = 0;
+
+		while(true) {
+			try {
+				System.out.print("Digite o minuto (0-59): ");
+				minuto = ler.nextInt();
+				if (minuto < 0 || minuto > 59) {
+					System.out.println("Hora inválida. Deve estar entre 0 e 59. Tente novamente:");
+				} else {
+					return minuto;
+				}
+			} catch (InputMismatchException error) {
+				System.out.println("Digite um número inteiro válido.");
+				ler.next();
+			}
+		}
+	}
+
+	public static int lerSegundo(Scanner ler) {
+		int segundo = 0;
+
+		while(true) {
+			try {
+				System.out.print("Digite o segundo (0-59): ");
+				segundo = ler.nextInt();
+				if (segundo < 0 || segundo > 59) {
+					System.out.println("Hora inválida. Deve estar entre 0 e 59. Tente novamente:");
+				} else {
+					return segundo;
+				}
+			} catch (InputMismatchException error) {
+				System.out.println("Digite um número inteiro válido.");
+				ler.next();
+			}
+		}
+	}
+
 	public static void main (String [] args) {
+		// Criação do objeto Scanner
 		Scanner ler = new Scanner(System.in);
+
+		// Inicialização das variáveis
+		int h = lerHora(ler);
+		int m = lerMinuto(ler);
+		int s = lerSegundo(ler);
+
+		// Criação do objeto Hora
+		Hora hora = new Hora(h, m, s);
+
+		// Exibição dos dados
+		System.out.println(hora.getHora1());
+		System.out.println(hora.getHora2());
+		System.out.println(hora.getSegundos() + " segundos.");
 	}
 }
